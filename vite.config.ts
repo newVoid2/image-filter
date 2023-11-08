bea5d8d7f563c44f89bd55957619f2c4dd4d7b51
Richard <rkerr022@gmail.com>
2023-11-08 13:50:53 -0500

    adding template and tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePluginFonts } from 'vite-plugin-fonts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePluginFonts({
+      google: {
+        families: [{
+          name: "Quicksand",
+        }]
+      }
+    })
   ],
   resolve: {
     alias: {
